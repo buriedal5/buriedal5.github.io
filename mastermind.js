9506a951e58aa218fe45efa0af946e52f5a25e79
buriedal5 <thomnelson@gmail.com>
2016-04-24 14:11:00 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -54,6 +54,12 @@ function displayGame(numRows) {
   for (var i = 0; i < colors.length; ++i) {
     var copy = template.cloneNode(true);
     copy.style.background = colors[i];
+    copy.children[i].onclick = (function() {
+      var color = i;
+      return function() {
+        currentColor = color;
+      };
+    })();
     container.appendChild(copy);
   }
   container.removeChild(template);
