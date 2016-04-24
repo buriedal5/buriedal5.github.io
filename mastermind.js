1efb0b57d48018cca9e3f2afc8fab9317e04e5bb
buriedal5 <thomnelson@gmail.com>
2016-04-24 12:01:50 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -1,3 +1,13 @@
 function clickPeg(row, column) {
   window.alert("click " + row + ":" + column);
 }
+
+function displayGame(numRows) {
+  var container = document.getElementById("game-container");
+  var template = container.firstChild;
+  for (var i = 0; i < numRows; ++i) {
+    var copy = template.cloneNode();
+    container.appendChild(copy);
+  }
+  container.removeChild(template);
+}
